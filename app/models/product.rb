764a5c442f2c899e72182bf52da238b651f35be9
Moises Ariza <arizamoises@gmail.com>
2022-06-29 14:55:50 -0500

    Creation the Products model, partial and helper

---
+++ app/models/product.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :string
+#  visible     :boolean          default(TRUE)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Product < ApplicationRecord
+end
