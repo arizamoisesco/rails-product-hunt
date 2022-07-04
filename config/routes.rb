f36364799f2afd8ac0933b3d0ee2d3017c37dbdb
Moises Ariza <arizamoises@gmail.com>
2022-07-04 09:49:35 -0500

    Use resources helper for drive routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  get 'products', to: 'products#index'
-  get 'products/new', to: 'products#new', as: 'new_product'
+  # get 'products', to: 'products#index'
+  #get 'products/new', to: 'products#new', as: 'new_product'
 
-  get 'products/:id', to: 'products#show', as: :product
+  #get 'products/:id', to: 'products#show', as: :product
 
-  get 'products/:id/edit', to: 'products#edit', as: :edit_product
-  patch 'products/:id', to: 'products#update'
+  #get 'products/:id/edit', to: 'products#edit', as: :edit_product
+  #patch 'products/:id', to: 'products#update'
 
 
-  post 'products', to: 'products#create'
+  #post 'products', to: 'products#create'
+
+  resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
